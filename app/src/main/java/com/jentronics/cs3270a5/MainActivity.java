32af38d0b6adef2a473a4217f24c64e660156981
Ron Jensen <ron@jentronics.com>
2018-09-30 20:02:38 -0600

    End of second video

--- app/src/main/java/com/jentronics/cs3270a5/MainActivity.java
+++ app/src/main/java/com/jentronics/cs3270a5/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
@@ -18,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button btnShow = (Button) findViewById(R.id.btn_test);
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimeOutDialogFragment dialog = new TimeOutDialogFragment();
+                dialog.setCancelable(false);
+                dialog.show(getSupportFragmentManager(), "timeoutDialog");
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/jentronics/cs3270a5/TimeOutDialogFragment.java
@@ -0,0 +1,53 @@
+package com.jentronics.cs3270a5;
+
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimeOutDialogFragment extends DialogFragment {
+
+
+    public TimeOutDialogFragment() {
+        // Required empty public constructor
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity() );
+
+        builder.setMessage(R.string.diag_time_body)
+                .setTitle(R.string.diag_time_title)
+                .setPositiveButton(R.string.btn_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.d("TODO","What happens after timeout?");
+                    }
+                });
+
+        return builder.create(); //super.onCreateDialog(savedInstanceState);
+    }
+
+/*
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_time_out_dialog, container, false);
+    }
+*/
+
+}
